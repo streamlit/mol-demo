a85508bcd78a2478d16f2a18c02f5826f814ce6c
sfc-gh-mnowotka <michal.nowotka@snowflake.com>
2023-03-16 07:11:48 +0100

    Long smiles eliminated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,28 @@
 import pandas as pd
 import streamlit as st
+from typing import Optional
 from streamlit_ketcher import st_ketcher
 from chembl_webresource_client.new_client import new_client as ch
 
 ch.image.set_format('svg')
-DEFAULT_MOL = "C[N+]1=CC=C(/C2=C3\C=CC(=N3)/C(C3=CC=CC(C(N)=O)=C3)=C3/C=C/C(=C(\C4=CC=[N+](C)C=C4)C4=N/C(=C(/C5=CC=CC(C(N)=O)=C5)C5=CC=C2N5)C=C4)N3)C=C1"
+DEFAULT_COMPOUND = "CHEMBL141739"
 EBI_URL = "https://www.ebi.ac.uk/chembl/"
 
 
 def name_to_smiles(name: str) -> str:
     ret = ch.molecule.filter(molecule_synonyms__molecule_synonym__iexact=name).only('molecule_structures')
-    return ret[0]["molecule_structures"]["canonical_smiles"]
+    return ret[0]["molecule_structures"]["molfile"]
 
 
-def render_similarity_table(smiles: str, threshold: int) -> str:
+def render_similarity_table(smiles: str, threshold: int) -> Optional[str]:
     colummns = ['molecule_chembl_id', 'similarity', 'pref_name']
-    ret = ch.similarity.filter(smiles=smiles, similarity=threshold).only(colummns)
+    try:
+        ret = ch.similarity.filter(smiles=smiles, similarity=threshold).only(colummns)
+    except Exception as _:
+        ret = None
+    if not ret:
+        st.warning("No results found")
+        return
     ret = [
         {
             "Similarity": float(x["similarity"]),
@@ -35,14 +42,14 @@ def render_similarity_table(smiles: str, threshold: int) -> str:
 
 
 if "smiles" not in st.session_state:
-    st.session_state.smiles = DEFAULT_MOL
+    st.session_state.smiles = None
 
 st.set_page_config(layout="wide")
 st.subheader("🧪 Molecule editor")
 
-molecule = st.text_input(
-    "Molecule from [SMILES](https://en.wikipedia.org/wiki/Simplified_molecular-input_line-entry_system) string:",
-    DEFAULT_MOL)
+chembl_id = st.text_input("ChEMBL ID:", DEFAULT_COMPOUND)
+st.session_state.smiles = ch.molecule.filter(chembl_id=chembl_id).only('molecule_structures')[0]["molecule_structures"]["molfile"]
+
 col1, col2, col3 = st.columns(3)
 with col1:
     if st.button('☕ Caffeine'):
@@ -55,10 +62,11 @@ with col3:
         st.session_state.smiles = name_to_smiles('Nicotine')
 editor_column, results_column = st.columns(2)
 with editor_column:
-    st.session_state["smiles"] = st_ketcher(st.session_state.smiles)
-    st.markdown(f"`{st.session_state.smiles}`")
+    smiles = st_ketcher(st.session_state.smiles)
     similarity_threshold = st.slider("Similarity threshold:", min_value=60, max_value=100)
+    st.markdown(f"```{smiles}```")
     with results_column:
-        table = render_similarity_table(st.session_state.smiles, similarity_threshold)
-        st.markdown(f'<div id="" style="overflow:scroll; height:600px;">{table}</div>', unsafe_allow_html=True)
+        table = render_similarity_table(smiles, similarity_threshold)
+        if table:
+            st.markdown(f'<div id="" style="overflow:scroll; height:600px;">{table}</div>', unsafe_allow_html=True)
 
